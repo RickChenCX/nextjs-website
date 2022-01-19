a6ae84e101c76930bee68d1b48e33a62b1bd862d
Rick chen <chunxiang.chen@REInventVentures.com>
2022-01-19 10:28:49 +0800

    feat: add page title and desc

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,5 +1,6 @@
 import type { ReactElement } from "react";
 import Layout from "components/Layout";
+import Head from "next/head";
 import styles from "./index.module.css";
 import Image from "next/image";
 
@@ -42,105 +43,120 @@ export default function About() {
     },
   ];
   return (
-    <main className={`w-full`}>
-      <div
-        className={`${styles.banner} bg-center bg-cover bg-no-repeat relative xl:bg-about-banner-pc xl:pt-39 xl:h-300 xl:pl-151 xs:bg-about-banner-mobile xs:h-336 xs:pl-8 xs:pt-215`}
-      >
-        <h1 className={`text-5xxl`}>About Us</h1>
-      </div>
-      <div
-        className={`${styles.article} bg-center bg-cover bg-no-repeat relative flex flex-col justify-start xl:bg-about-article-pc xl:pt-20 xl:h-1324 xl:px-151 xs:bg-about-article-mobile xs:h-1152 xs:px-4 xs:pt-0`}
-      >
-        <div className={`xl:w-592`}>
-          <h3 className={`text-neutral9 xl:text-xlx xl:pb-4 xs:pt-10`}>
-            Who we are
-          </h3>
-          <h4
-            className={`text-neutral2 xl:text-5xxl xl:pb-8 xs:text-3xxl xs:pb-4`}
-          >
-            We are a <span>SaaS provider</span> with deep roots in the
-            <span> prop-tech</span> industry.
-          </h4>
-          <h5 className={`text-neutral12 text-base xs:pb-4`}>
-            Connected business modules ensure your business efficiency and
-            growth. We invest talent and resources into a repeatable process
-            that outputs valuable companies.
-          </h5>
+    <div className="w-full h-full">
+      <Head>
+        <title>About BOSS - Proptech Software and Solution Provider</title>
+        <meta
+          name="description"
+          content="We provide the most powerful software to help you manage your coworking, flex and hybrid workplaces around the world."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=0"
+        />
+      </Head>
+      <main className={`w-full`}>
+        <div
+          className={`${styles.banner} bg-center bg-cover bg-no-repeat relative xl:bg-about-banner-pc xl:pt-39 xl:h-300 xl:pl-151 xs:bg-about-banner-mobile xs:h-336 xs:pl-8 xs:pt-215`}
+        >
+          <h1 className={`text-5xxl`}>About Us</h1>
         </div>
-        <div className={`xl:w-592 xl:self-end`}>
-          <h3 className={`text-neutral9 xl:text-xlx xl:pb-4 xs:pt-10`}>
-            Our Vision
-          </h3>
-          <h4
-            className={`text-neutral2 xl:text-5xxl xl:pb-8 xs:text-3xxl xs:pb-4`}
-          >
-            BOSS is the <span>solution.</span>
-          </h4>
-          <h5 className={`text-neutral12 text-2xl xl:pb-6 xs:pb-2`}>
-            BOSS = Business Operating System for Space
-          </h5>
-          <h5 className={`text-neutral12 text-base xs:pb-4`}>
-            BOSS helps you to automate workspace processes, manage tenant
-            lifecycles and streamline flex & workspace operations. With BOSS,
-            you can strengthen a gratifying customer experience and boost
-            retention. In turn, maximize profitability while ensuring accuracy
-            and efficiency.
-          </h5>
-        </div>
-        <div className={`xl:w-592`}>
-          <h3 className={`text-neutral9 xl:text-xlx xl:pb-4 xs:pt-10`}>
-            Our Mission
-          </h3>
-          <h4
-            className={`text-neutral2 xl:text-5xxl xl:pb-8 xs:text-3xxl xs:pb-8`}
-          >
-            Unified business modules ensure your business
-            <span>efficiency</span> and <span>growth.</span>
-          </h4>
-          <h5 className={`text-neutral12 text-base xs:pb-4`}>
-            BOSS’s mission is to improve the world by reinventing real estate
-            and how people use it by using our strengths in technology
-            innovation, venture building, and business model innovation.
-          </h5>
-        </div>
-      </div>
-      <div
-        className={`${styles.teamGroup} xl:px-151 xl:pt-16 xl:pb-14 xs:pt-10 xs:px-4`}
-      >
-        <h2 className={`xl:text-5xxl pb-8 xl:w-600 xs:text-3xxl`}>
-          Leadership team with well-rounded expertises
-        </h2>
-        <p className={`text-base xl:w-600`}>
-          BOSS’s management team has a proven track record in PropTech in Asia,
-          having served as the leadership team of some of the most
-          technology-forward real estate brands in Asia prior to starting BOSS.
-        </p>
-      </div>
-      <div
-        className={`${styles.brandTitle} text-2xl xl:pt-14 xl:pb-6 xs:px-4 xs:py-8`}
-      >
-        Leadership Team&apos;s Experiences
-      </div>
-      <div
-        className={`${styles.brand} xl:grid xl:grid-cols-7 xl:h-50 xl:px-151 xl:pb-28 xs:flex xs:flex-wrap xs:justify-around xs:items-center xs:px-4 xs:pb-10 xs:h-306`}
-      >
-        {brandList.map((item) => {
-          return (
-            <a
-              key={item.localUrl}
-              className={`${styles.img} text-center xl:relative`}
+        <div
+          className={`${styles.article} bg-center bg-cover bg-no-repeat relative flex flex-col justify-start xl:bg-about-article-pc xl:pt-20 xl:h-1324 xl:px-151 xs:bg-about-article-mobile xs:h-1152 xs:px-4 xs:pt-0`}
+        >
+          <div className={`xl:w-592`}>
+            <h3 className={`text-neutral9 xl:text-xlx xl:pb-4 xs:pt-10`}>
+              Who we are
+            </h3>
+            <h4
+              className={`text-neutral2 xl:text-5xxl xl:pb-8 xs:text-3xxl xs:pb-4`}
             >
-              <Image
-                src={`/images/${item.localUrl}.jpg`}
-                width={130}
-                height={50}
-                alt={item.alt}
-              />
-            </a>
-          );
-        })}
-      </div>
-    </main>
+              We are a <span>SaaS provider</span> with deep roots in the
+              <span> prop-tech</span> industry.
+            </h4>
+            <h5 className={`text-neutral12 text-base xs:pb-4`}>
+              Connected business modules ensure your business efficiency and
+              growth. We invest talent and resources into a repeatable process
+              that outputs valuable companies.
+            </h5>
+          </div>
+          <div className={`xl:w-592 xl:self-end`}>
+            <h3 className={`text-neutral9 xl:text-xlx xl:pb-4 xs:pt-10`}>
+              Our Vision
+            </h3>
+            <h4
+              className={`text-neutral2 xl:text-5xxl xl:pb-8 xs:text-3xxl xs:pb-4`}
+            >
+              BOSS is the <span>solution.</span>
+            </h4>
+            <h5 className={`text-neutral12 text-2xl xl:pb-6 xs:pb-2`}>
+              BOSS = Business Operating System for Space
+            </h5>
+            <h5 className={`text-neutral12 text-base xs:pb-4`}>
+              BOSS helps you to automate workspace processes, manage tenant
+              lifecycles and streamline flex & workspace operations. With BOSS,
+              you can strengthen a gratifying customer experience and boost
+              retention. In turn, maximize profitability while ensuring accuracy
+              and efficiency.
+            </h5>
+          </div>
+          <div className={`xl:w-592`}>
+            <h3 className={`text-neutral9 xl:text-xlx xl:pb-4 xs:pt-10`}>
+              Our Mission
+            </h3>
+            <h4
+              className={`text-neutral2 xl:text-5xxl xl:pb-8 xs:text-3xxl xs:pb-8`}
+            >
+              Unified business modules ensure your business
+              <span>efficiency</span> and <span>growth.</span>
+            </h4>
+            <h5 className={`text-neutral12 text-base xs:pb-4`}>
+              BOSS’s mission is to improve the world by reinventing real estate
+              and how people use it by using our strengths in technology
+              innovation, venture building, and business model innovation.
+            </h5>
+          </div>
+        </div>
+        <div
+          className={`${styles.teamGroup} xl:px-151 xl:pt-16 xl:pb-14 xs:pt-10 xs:px-4`}
+        >
+          <h2 className={`xl:text-5xxl pb-8 xl:w-600 xs:text-3xxl`}>
+            Leadership team with well-rounded expertises
+          </h2>
+          <p className={`text-base xl:w-600`}>
+            BOSS’s management team has a proven track record in PropTech in
+            Asia, having served as the leadership team of some of the most
+            technology-forward real estate brands in Asia prior to starting
+            BOSS.
+          </p>
+        </div>
+        <div
+          className={`${styles.brandTitle} text-2xl xl:pt-14 xl:pb-6 xs:px-4 xs:py-8`}
+        >
+          Leadership Team&apos;s Experiences
+        </div>
+        <div
+          className={`${styles.brand} xl:grid xl:grid-cols-7 xl:h-50 xl:px-151 xl:pb-28 xs:flex xs:flex-wrap xs:justify-around xs:items-center xs:px-4 xs:pb-10 xs:h-306`}
+        >
+          {brandList.map((item) => {
+            return (
+              <a
+                key={item.localUrl}
+                className={`${styles.img} text-center xl:relative`}
+              >
+                <Image
+                  src={`/images/${item.localUrl}.jpg`}
+                  width={130}
+                  height={50}
+                  alt={item.alt}
+                />
+              </a>
+            );
+          })}
+        </div>
+      </main>
+    </div>
   );
 }
 About.getLayout = function getLayout(About: ReactElement) {
